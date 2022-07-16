921f2d20ad9bd5aa4394006a7cbc0f66357d4481
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-16 16:43:14 +0300

    Delete commands directory

--- commands/core/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'ping',
-    aliases: [],
-    utilisation: '{prefix}ping',
-
-    execute(client, message) {
-        message.channel.send(`**${client.ws.ping}ms** 🛰️`);
-    },
-};
