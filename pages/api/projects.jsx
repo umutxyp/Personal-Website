7fa2e899f7ae177db9f2a91d17350dbe68ba5447
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-21 09:27:43 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -10,10 +10,10 @@ export default async (req, res) => {
     },
     {
       id: 2,
-      image: "https://media.discordapp.net/attachments/737347015251460156/989673154727264296/channels4_profile.jpg",
+      image: "https://media.discordapp.net/attachments/737348411568685066/999563078079361074/YT_AVATAR_NET.jpg",
       name: "Youtube",
       description:
-        "My personal youtube channel where I post music videos.",
+        "I am a professional editor and intermediate software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management. I post videos about discord on my youtube channel.",
       link: "https://youtube.com/UmutBayraktarYT",
     },
      {
