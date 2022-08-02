abb2fd692c5de20a872794f34e04e6df6aa96576
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-02 18:04:17 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,7 +26,7 @@ export default function Home() {
             
                 </div>
                 <p className="text-white/50 text-md mt-3">
-I am a professional editor and intermediate software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management.
+I am a professional editor and middleware editor. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management.
                 </p>
               </div>
               <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
