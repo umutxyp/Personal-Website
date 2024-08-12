7024bf1e2ed2156133f524d9f99f8774e07c1275
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2024-08-12 06:16:41 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -2,17 +2,17 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://media.discordapp.net/attachments/737348411568685066/954502826791428136/codeshare_logo.png",
+      image: "https://codeshare.me/assets/img/favicon.png",
       name: "Code Share",
-      description: "Discord türkiyenin en büyük discord bot komutları arşivi ve en büyük discord botu uptime, hostlayıcı websitesi. Code Share veritabanında discord botlarına özgü yüzlerce discord bot komutu bulunmaktadır ve kendi discord botunuzu yaratmanıza yardımcı olur.",
+      description: "Code Share, it offers an ideal platform for developers to share their projects, interact with other developers and sell their projects for money, exploring many projects in different categories and software languages.",
       link: "https://codeshare.me",
     },
     {
       id: 2,
-      image: "https://media.discordapp.net/attachments/737348411568685066/999563078079361074/YT_AVATAR_NET.jpg",
-      name: "Youtube Channel",
+      image: "https://images-ext-1.discordapp.net/external/BagPBMpzrJCuIJKUSdmQ5CFusioIc9-clJBoObcUA0A/https/cdn.discordapp.com/avatars/615029465726320654/a_9a33f999daebb6e9197cf7bb6ff5f506.gif",
+      name: "Social Accounts",
       description: "I am a professional editor and intermediate software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management. I post videos about discord on my youtube channel.",
-      link: "https://youtube.com/UmutBayraktarYT",
+      link: "https://linktr.ee/umutxyp",
     }
   ];
   res.status(200).json(data);
