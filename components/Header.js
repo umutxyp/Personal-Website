6d85e23aa74ff2ca17035f4b08ce8dd72f3a344d
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2024-08-12 06:17:32 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 
 const items = [
     { icon: 'fab fa-youtube', link: 'https://youtube.com/UmutBayraktarYT' },
-    { icon: 'fab fa-twitch', link: 'https://twitch.com/umutyzx' },
+    { icon: 'fab fa-twitch', link: 'https://twitch.com/umutxyp' },
     { icon: 'fab fa-discord', link: 'https://discord.com/users/615029465726320654' },
     { icon: 'fab fa-instagram', link: 'https://instagram.com/umutxyp' },
     { icon: 'fab fa-facebook', link: 'https://facebook.com/umutxyp' },
@@ -12,7 +12,7 @@ const navItems = [
     { icon: 'fal fa-home', active: 'fa fa-home', label: 'Home', href: '/' },
     { icon: 'fal fa-compass', active: 'fa fa-compass', label: 'Projects', href: '/projects' },
     { icon: 'fal fa-phone', active: 'fa fa-phone', label: 'Contact', href: '/contact' },
-    { icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Blogs', href: 'https://blog.umut.live' }
+    { icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Blogs', href: 'https://blog.codeshare.me' }
 ]
 
 import Link from 'next/link';
