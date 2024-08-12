e8dc06cce2f4c0736cd43bece928b2ca8d112cca
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2024-08-12 06:22:12 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -30,7 +30,7 @@ I am a professional editor and software developer. I have been professionally in
                 </p>
               </div>
               <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
-                <img alt="umutbayraktar" src={`https://media.discordapp.net/attachments/737348411568685066/996841172372824184/YT_AVATAR_NET.jpg`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
+                <img alt="umutbayraktar" src={`https://images-ext-1.discordapp.net/external/BagPBMpzrJCuIJKUSdmQ5CFusioIc9-clJBoObcUA0A/https/cdn.discordapp.com/avatars/615029465726320654/a_9a33f999daebb6e9197cf7bb6ff5f506.gif`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
                 <div className={`bg-[#040404] rounded-full px-[4px] py-[1px] flex items-center absolute bottom-0 right-4`}>
                   <Tippy content="Online" animation="shift-away" arrow={false}>
                     <i className={`fad fa-circle text-2xl text-online`} />
@@ -40,90 +40,8 @@ I am a professional editor and software developer. I have been professionally in
               
             </div>
             <br></br>
-            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">Community Manager</span>
+            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">Content Creator / Manager</span>
           </div>
-      </div>
-
-
-        <div className="py-20">
-        <p className="text-3xl text-white font-semibold">GitHub Repositories</p>
-
-        <div className="grid grid-cols-1 md:grid-cols-3 w-full gap-4 items-center mt-2">
-          {_repositories
-            ? repositories
-              ? repositories
-                  .sort((a, b) => b.stargazers_count - a.stargazers_count)
-                  .map((_, __) => (
-                    <a
-                      key={__}
-                      href={`https://github.com/umutxyp/`+_.name}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="bg-[#191932]/20 p-4 hover:bg-[#191932]/30 shadow-lg hover:shadow-xl transition-all duration-200 rounded-lg w-full"
-                    >
-                      <p className="text-md text-white">
-                        <span className="text-sm text-white/50 bg-black/25 px-2 py-1 rounded-md mr-1">
-                          {_.owner.login}
-                        </span>
-                        {_.name}
-                      </p>
-                      <div className="mt-5 flex justify-end w-full h-full items-center">
-                        <div className="flex w-full justify-between items-center">
-                          <Tippy
-                            content={"Stars"}
-                            arrow={false}
-                            animation="shift-away"
-                          >
-                            <div className="flex items-center">
-                              <p className="text-sm">
-                                <i className="fal fa-star mr-2" />
-                              </p>
-                              <p>{_.stargazers_count}</p>
-                            </div>
-                          </Tippy>
-                          <div className="text-sm text-white bg-black/25 px-2 py-1 rounded-md mr-1">
-                            {_.language || "Empty"}
-                          </div>
-                          <Tippy
-                            content={"Forks"}
-                            arrow={false}
-                            animation="shift-away"
-                          >
-                            <div className="flex items-center justify-end">
-                              <p>{_.forks}</p>
-                              <p className="text-sm">
-                                <i className="fal fa-code-branch ml-2" />
-                              </p>
-                            </div>
-                          </Tippy>
-                        </div>
-                      </div>
-                    </a>
-                  ))
-              : Array.from({ length: 6 }).map((_, __) => (
-                  <div
-                    key={__}
-                    className="bg-[#191932]/20 p-4 rounded-lg w-full"
-                  >
-                    <div className="bg-[#191932]/50 animate-pulse w-full h-[28px] rounded-md" />
-                    <div className="mt-5 flex w-full justify-between items-center">
-                      <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
-                      <div className="bg-[#191932]/50 animate-pulse w-24 h-[24px] rounded-md" />
-                      <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
-                    </div>
-                  </div>
-                ))
-            : Array.from({ length: 6 }).map((_, __) => (
-                <div key={__} className="bg-[#191932]/20 p-4 rounded-lg w-full">
-                  <div className="bg-[#191932]/50 animate-pulse w-full h-[28px] rounded-md" />
-                  <div className="mt-5 flex w-full justify-between items-center">
-                    <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
-                    <div className="bg-[#191932]/50 animate-pulse w-24 h-[24px] rounded-md" />
-                    <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
-                  </div>
-                </div>
-              ))}
-        </div>
       </div>
 
 
