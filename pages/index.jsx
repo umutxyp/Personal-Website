72a38023518f8f538f3a305bdeffcecdf61eba55
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2024-08-12 07:04:54 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,11 +18,6 @@ export default function Home() {
                 <p className="flex items-center text-white text-4xl font-semibold">
                   Umut Bayraktar
                 </p>
-                  <Tippy content={`Online`} animation="shift-away" arrow={false}>
-                    <span className={`ml-2 text-online px-2 py-1 font-normal rounded-md text-sm`}>
-                          <i className={`fa fa-circle text-online mr-2`} />Online
-                    </span>
-                  </Tippy>
             
                 </div>
                 <p className="text-white/50 text-md mt-3">
@@ -30,12 +25,28 @@ I am a professional editor and software developer. I have been professionally in
                 </p>
               </div>
               <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
-                <img alt="umutbayraktar" src={`https://images-ext-1.discordapp.net/external/BagPBMpzrJCuIJKUSdmQ5CFusioIc9-clJBoObcUA0A/https/cdn.discordapp.com/avatars/615029465726320654/a_9a33f999daebb6e9197cf7bb6ff5f506.gif`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
-                <div className={`bg-[#040404] rounded-full px-[4px] py-[1px] flex items-center absolute bottom-0 right-4`}>
-                  <Tippy content="Online" animation="shift-away" arrow={false}>
-                    <i className={`fad fa-circle text-2xl text-online`} />
-                  </Tippy>
+                <img alt="umutbayraktar" src={`https://ugc.production.linktr.ee/6RJk9s2pQZ2yAdcxw3Ir_TXX2dLlNmwM2OFdf?io=true&size=avatar-v3_0`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
+              </div>
+              
+            </div>
+            <br></br>
+            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">Content Creator / Manager</span>
+          </div>
+      </div>
+
+        <div className="bg-neutral-800/10 shadow-xl rounded-lg w-full h-auto mt-6">
+          <div className="relative">
+            <div className="flex flex-col lg:flex-row justify-between w-full p-6 px-8 items-center h-full">
+              <div className="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full">
+                <div className="flex items-center">
+                <p className="flex items-center text-white text-4xl font-semibold">
+                  About Me
+                </p>
+            
                 </div>
+                <p className="text-white/50 text-md mt-3">
+                  I'm a 19-year-old living in London, England. I am originally from Turkey. I was born in Antalya. My first project is <a href="https://codeshare.me">Code Share</a>, a public software publishing platform. I have long term experience in community management and general project planning. I am a professional content producer and have more than 55k followers in the field of software and cryptocurrency on social media. I am a fullstack web developer, but as of 2024, I am not actively developing a full project. 
+                </p>
               </div>
               
             </div>
