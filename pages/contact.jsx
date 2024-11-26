4f22df780a0af750e354c7bd3e4acb6d84083928
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2024-11-26 22:01:02 +0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -14,7 +14,7 @@ export default function Home() {
         <p className="text-xl text-white/50 font-normal text-center mb-5">Other addresses where you can contact me.</p>
         <div className=" flex justify-center flex-col items-center">
             <div className="max-w-lg w-full">
-            <a target="_blank" rel="norefferer" href="https://discord.com/users/615029465726320654">
+            <a target="_blank" rel="norefferer" href="https://discord.com/users/518341959765458985">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-discord fa-2x" />
                         <p className="font-semibold text-xl">@umutxyp</p>
